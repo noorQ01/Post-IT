820c282ad83779a359941dd9ab0c6ac7d244fae6
noorQ01 <noorqatan01@gmail.com>
2024-12-04 08:46:00 +0400

    update postSlice

--- client/src/Features/PostSlice.js
+++ client/src/Features/PostSlice.js
@@ -9,7 +9,7 @@ const initialState = {
 
 export const savePost = createAsyncThunk("posts/savePost", async (postData) => {
   try {
-    const response = await axios.post("http://localhost:3001/savePost", {
+    const response = await axios.post(`${ENV.SERVER_URL}/savePost`, {
       postMsg: postData.postMsg,
       email: postData.email,
     });
@@ -22,7 +22,7 @@ export const savePost = createAsyncThunk("posts/savePost", async (postData) => {
 
 export const getPosts = createAsyncThunk("post/getPosts", async () => {
   try {
-    const response = await axios.get("http://localhost:3001/getPosts");
+    const response = await axios.get(`${ENV.SERVER_URL}/getPosts`);
     return response.data.posts;
     console.log(response);
   } catch (error) {
@@ -33,7 +33,7 @@ export const getPosts = createAsyncThunk("post/getPosts", async () => {
 export const likePost = createAsyncThunk("posts/likePost", async (postData) => {
   try {
     const response = await axios.put(
-      `http://localhost:3001/likePost/${postData.postId}`,
+      `${ENV.SERVER_URL}/likePost/${postData.postId}`,
       {
         userId: postData.userId,
       }
